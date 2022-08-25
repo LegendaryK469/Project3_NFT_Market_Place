23dd735218d2df8d9ab02a96856926ba5783586e
Juan <jsbohorquez92@gmail.com>
2022-08-24 23:12:44 -0500

    completed file for create-transaction.js file, formally create-token.js

--- pages/create-transaction.js
+++ pages/create-transaction.js
@@ -1,4 +1,7 @@
-async function createToken(url) {
+// This function will mint an NFT and subsequently place it in
+// the market place for sale.
+
+async function createTransaction(url) {
     const web3Modal = new Web3Modal({
         network: "mainnet",
         cacheProvider: true,
@@ -9,6 +12,8 @@ async function createToken(url) {
     const signer = source.getSigner()
 
     let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
+
+    // the 'createToken' function allows for minting of NFT
     let transaction = await contract.createToken(url)
     let tx = await transaction.wait()
     let event =tx.events[0]
@@ -16,6 +21,10 @@ async function createToken(url) {
     let tokenId = value.toNumber()
     const price = web3.utils.toWei(formInput.price, 'ether')
 
-    
+    // 'createMarketItem' will place newly minted NFT for sale
+    contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    transaction = await contract.createMarketItem(nftaddress, tokenId, price)
+    await transaction.wait()
+    router.push('/')
 
 }
